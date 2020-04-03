b4294ef16cdbf0130c7f83bfca0e7bdd4b21fa0a
MechroCat22 <achasedev@gmail.com>
2020-04-03 15:44:39 -0700

    Added StepTimer

---
+++ Source/Engine/Time/StepTimer.cpp
@@ -0,0 +1,186 @@
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+/// Author: Andrew Chase
+/// Date Created: April 3rd, 2020
+/// Description: 
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+#pragma once
+
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+/// INCLUDES
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+#include "Engine/Framework/EngineCommon.h"
+#include "Engine/Math/MathUtils.h"
+#include "Engine/Time/Clock.h"
+#include "Engine/Time/StepTimer.h"
+#include "Engine/Time/Time.h"
+
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+/// DEFINES
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+/// ENUMS, TYPEDEFS, STRUCTS, FORWARD DECLARATIONS
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+/// GLOBALS AND STATICS
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+/// C FUNCTIONS
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+/// CLASS IMPLEMENTATIONS
+///--------------------------------------------------------------------------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------------------
+StepTimer::StepTimer()
+{
+	SetClock(nullptr);
+	Reset();
+}
+
+
+//-------------------------------------------------------------------------------------------------
+StepTimer::StepTimer(Clock* referenceClock)
+{
+	SetClock(referenceClock);
+	Reset();
+}
+
+
+//-------------------------------------------------------------------------------------------------
+void StepTimer::Reset()
+{
+	m_startHPC = m_referenceClock->GetTotalHPC();
+	m_endHPC = m_startHPC;
+}
+
+
+//-------------------------------------------------------------------------------------------------
+void StepTimer::SetClock(Clock* clock)
+{
+	m_referenceClock = (clock != nullptr ? clock : Clock::GetMasterClock());
+}
+
+
+//-------------------------------------------------------------------------------------------------
+void StepTimer::SetInterval(float seconds)
+{
+	uint64 interval = TimeSystem::SecondsToPerformanceCount(seconds);
+	m_startHPC = m_referenceClock->GetTotalHPC();
+	m_endHPC = m_startHPC + interval;
+}
+
+
+//-------------------------------------------------------------------------------------------------
+void StepTimer::SetElapsedTime(float secondsElapsed)
+{
+	// Save off the interval length to preserve it
+	uint64 intervalLength = m_endHPC - m_startHPC;
+
+	uint64 elapsedHPC = TimeSystem::SecondsToPerformanceCount(secondsElapsed);
+	uint64 currentHPC = m_referenceClock->GetTotalHPC();
+
+	m_startHPC = currentHPC - elapsedHPC;
+	m_endHPC = m_startHPC + intervalLength;
+}
+
+
+//-------------------------------------------------------------------------------------------------
+bool StepTimer::CheckAndReset()
+{
+	if (HasIntervalElapsed())
+	{
+		Reset();
+		return true;
+	}
+
+	return false;
+}
+
+
+//-------------------------------------------------------------------------------------------------
+bool StepTimer::DecrementByIntervalOnce()
+{
+	if (HasIntervalElapsed())
+	{
+		uint64 interval = m_endHPC - m_startHPC;
+
+		m_startHPC += interval;
+		m_endHPC += interval;
+		return true;
+	}
+
+	return false;
+}
+
+
+//-------------------------------------------------------------------------------------------------
+int StepTimer::DecrementByIntervalAll()
+{
+	uint64 currentHPC = m_referenceClock->GetTotalHPC();
+	uint64 interval = m_endHPC - m_startHPC;
+
+	int numElapses = 0;
+	while (m_endHPC < currentHPC)
+	{
+		m_startHPC += interval;
+		m_endHPC += interval;
+		numElapses++;
+	}
+
+	return numElapses;
+}
+
+
+//-------------------------------------------------------------------------------------------------
+float StepTimer::GetElapsedSeconds() const
+{
+	uint64 currentHPC = m_referenceClock->GetTotalHPC();
+	uint64 elapsedHPC = currentHPC - m_startHPC;
+
+	return static_cast<float>(TimeSystem::PerformanceCountToSeconds(elapsedHPC));
+}
+
+
+//-------------------------------------------------------------------------------------------------
+float StepTimer::GetElapsedTimeNormalized() const
+{
+	float elapsedSeconds = GetElapsedSeconds();
+	float intervalSeconds = static_cast<float>(TimeSystem::PerformanceCountToSeconds(m_endHPC - m_startHPC));
+
+	return (elapsedSeconds / intervalSeconds);
+}
+
+
+//-------------------------------------------------------------------------------------------------
+float StepTimer::GetSecondsUntilIntervalEnds() const
+{
+	uint64 currentHPC = m_referenceClock->GetTotalHPC();
+
+	float currentSeconds = (float)TimeSystem::PerformanceCountToSeconds(currentHPC);
+	float endSeconds = (float)TimeSystem::PerformanceCountToSeconds(m_endHPC);
+
+	float diff = endSeconds - currentSeconds;
+	float timeUntilIntervalEnds = Clamp(diff, 0.f, diff);
+
+	return timeUntilIntervalEnds;
+}
+
+
+//-------------------------------------------------------------------------------------------------
+bool StepTimer::HasIntervalElapsed() const
+{
+	uint64 currentHPC = m_referenceClock->GetTotalHPC();
+
+	return (currentHPC >= m_endHPC);
+}
+
+
+//-------------------------------------------------------------------------------------------------
+float StepTimer::GetDeltaSeconds() const
+{
+	return m_referenceClock->GetDeltaSeconds();
+}
