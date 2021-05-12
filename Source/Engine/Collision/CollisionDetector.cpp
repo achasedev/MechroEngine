80862dbc966ae67a4f5ae90c5ee379a40df78d5b
MechroCat22 <achasedev@gmail.com>
2021-05-11 21:57:44 -0400

    Added box-box edge-edge contact generation.

--- Source/Engine/Collision/CollisionDetector.cpp
+++ Source/Engine/Collision/CollisionDetector.cpp
@@ -186,11 +186,24 @@ int CollisionDetector::GenerateContacts(const BoxCollider& box, const SphereColl
 	return 1;
 }
 
+//-------------------------------------------------------------------------------------------------
+// Returns the float magnitude of how much the extents, defined in the basis' space, project onto the world-space axis
+static float ProjectExtentsOntoAxis(const Vector3& extents, const Matrix3& basis, const Vector3& axis)
+{
+	return extents.x * Abs(DotProduct(basis.iBasis, axis)) + extents.y * Abs(DotProduct(basis.jBasis, axis)) + extents.z + Abs(DotProduct(basis.kBasis, axis));
+}
+
 
 //-------------------------------------------------------------------------------------------------
-static float GetPenetrationOnAxis(const OBB3& boxA, const OBB3& boxB, const Vector3& axis, const Vector3& aToB)
+// Determines how much each box's extent projects onto the axis and how much the distance projects
+// If the distance is greater than the half extents, then the result is negative, indicating there must be a gap between the two
+static float GetPenetrationOnAxis(const Matrix3& aBasis, const Matrix3& bBasis, const Vector3& aExtents, const Vector3& bExtents, const Vector3& axis, const Vector3& aToB)
 {
+	float projA = ProjectExtentsOntoAxis(aExtents, aBasis, axis);
+	float projB = ProjectExtentsOntoAxis(bExtents, bBasis, axis);
+	float projDistance = Abs(DotProduct(aToB, axis));
 
+	return projA + projB - projDistance;
 }
 
 
@@ -272,7 +285,7 @@ int CollisionDetector::GenerateContacts(const BoxCollider& a, const BoxCollider&
 		if (AreMostlyEqual(axis.GetLengthSquared(), 0.f))
 			continue;
 
-		float penOnAxis = GetPenetrationOnAxis(boxAWs, boxBWs, axis, aToB);
+		float penOnAxis = GetPenetrationOnAxis(aBasis, bBasis, boxAWs.extents, boxBWs.extents, axis, aToB);
 		if (penOnAxis < 0.f)
 			return 0;
 
@@ -298,5 +311,83 @@ int CollisionDetector::GenerateContacts(const BoxCollider& a, const BoxCollider&
 	else
 	{
 		// It's an edge-edge contact - find out which two edges it is
+
+		// Get the local index of the basis vectors that created the cross product axis
+		// So 0 is i, 1 is j, 2 is k, for each box
+		int aAxisIndex = (bestAxisIndex - 6) / 3;
+		int bAxisIndex = (bestAxisIndex - 6) % 3;
+
+		// Start with the point at the max extents
+		Vector3 edgeAMidpoint = boxAWs.extents;
+		Vector3 edgeBMidpoint = boxBWs.extents;
+
+		// Get the axis of min separation
+		// Make sure the axis points from B to A
+		Vector3 seperatingAxis = axes[bestAxisIndex];
+
+		if (DotProduct(seperatingAxis, aToB) > 0.f)
+		{
+			seperatingAxis *= -1.0f;
+		}
+
+		// Get the direction of the edges while we're at it, to find the end points
+		Vector3 edgeADir = Vector3::ZERO;
+		Vector3 edgeBDir = Vector3::ZERO;
+
+		// Iterate across the 1st, 2nd, and 3rd basis vector of each box
+		for (int i = 0; i < 3; ++i)
+		{
+			// For the basis vector that contributed to the cross product yielding the separating axis, set that corresponding element to 0.f
+			// This places the point in the middle of that edge of the box (instead of +extent or -extent along that basis vector)
+			if (i == aAxisIndex)
+			{
+				edgeAMidpoint.data[i] = 0.f;
+				edgeADir.data[i] = boxAWs.extents.data[i]; // going to -value to +value of this component traces the edge
+			}
+			else if (DotProduct(axes[i], seperatingAxis) > 0.f) // For all other components, choose the ones furthest against the axis (towards B)
+			{		
+				edgeAMidpoint.data[i] = -1.0f * edgeAMidpoint.data[i];
+			}
+
+			// Do the same for B...
+			if (i == bAxisIndex)
+			{
+				edgeBMidpoint.data[i] = 0.f;
+				edgeBDir.data[i] = boxBWs.extents.data[i]; // going to -value to +value of this component traces the edge
+			}
+			else if (DotProduct(axes[i + 3], seperatingAxis) < 0.f) // ...except find the components most in the direction of the axis, towards A
+			{
+
+				edgeBMidpoint.data[i] = -1.0f * edgeBMidpoint.data[i];
+			}
+		}
+		
+		// Determine the edge endpoints in world space
+		Vector3 edgeAStartWs	= aBasis * (edgeAMidpoint - edgeADir) + boxAWs.center;
+		Vector3 edgeAEndWs		= aBasis * (edgeAMidpoint + edgeADir) + boxAWs.center;
+		Vector3 edgeBStartWs	= bBasis * (edgeBMidpoint - edgeBDir) + boxBWs.center;
+		Vector3 edgeBEndWs		= bBasis * (edgeBMidpoint + edgeBDir) + boxBWs.center;
+
+		// Get the closest points between the edges
+		Vector3 closestPtOnAWs;
+		Vector3 closestPtOnBWs;
+		FindClosestPointsOnLineSegments(edgeAStartWs, edgeAEndWs, edgeBStartWs, edgeBEndWs, closestPtOnAWs, closestPtOnBWs);
+
+		// Find the midpoint between the closest points
+		Vector3 finalMidpoint = 0.5f * (closestPtOnAWs - closestPtOnBWs) + closestPtOnBWs;
+
+		// Now make the contact
+		Contact* contact = &collisionData.contacts[collisionData.numContacts];
+
+		contact->position = finalMidpoint;
+		contact->normal = seperatingAxis;
+		contact->penetration = minPen;
+		contact->bodyA = a.GetOwnerRigidBody();
+		contact->bodyB = b.GetOwnerRigidBody();
+		contact->restitution = collisionData.restitution;
+		contact->friction = collisionData.friction;
+		collisionData.numContacts++;
 	}
+
+	return 1;
 }
